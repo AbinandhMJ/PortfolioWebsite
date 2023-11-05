54d13640faa3972897d52bb7f884d81060470a86
Abinandh MJ <99226172+AbinandhMJ@users.noreply.github.com>
2023-11-05 16:05:47 +0530

    Add files via upload
    Signed-off-by: Abinandh MJ  <99226172+AbinandhMJ@users.noreply.github.com>

---
+++ js/form.js
@@ -0,0 +1,16 @@
+const select = (selector) => document.querySelector(selector);
+
+const button = document.querySelector('.button');
+const submit = document.querySelector('.submit');
+
+function toggleClass() {
+  this.classList.toggle('active');
+}
+
+function addClass() {
+  this.classList.add('finished');
+}
+
+button.addEventListener('click', toggleClass);
+button.addEventListener('transitionend', toggleClass);
+button.addEventListener('transitionend', addClass);
